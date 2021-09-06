606089c0e1d559438facb9cad87f3b79fe036f1c
Mahdi Yusuf <myusuf3@bee.local>
2021-09-05 21:18:25 -0400

    getting external ip

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Sites struct {
+}
+
+func getDomains() (*Sites, error) {
+
+	return nil, nil
+}
+
+func get_external() string {
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	content, _ := ioutil.ReadAll(resp.Body)
+	return string(content)
+}
 
 func main() {
-	fmt.Println("Hello")
+	// domains, err := getDomains()
+
+	// get ip
+	ip := get_external()
+	fmt.Println("external ip: ", ip)
+
+	// for domains
 }
